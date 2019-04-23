d318be00cc080f241ea5fc4f8a300901d79264ff
Dan Marshall <danmar@microsoft.com>
2019-04-23 16:25:06 -0700

    allow choice of loading data into spec

--- packages/sanddance-explorer/src/dialogs/settings.tsx
+++ packages/sanddance-explorer/src/dialogs/settings.tsx
@@ -2,20 +2,43 @@
 // Licensed under the MIT license.
 import * as React from 'react';
 import { base } from '../base';
+import { DataFile } from '../interfaces';
 import { Dialog } from '../controls/dialog';
+import { Dropdown } from '../controls/dropdown';
 import { Explorer } from '../explorer';
+import { FabricTypes } from '@msrvida/office-ui-fabric-react-cdn-typings';
 import { Group } from '../controls/group';
+import {
+    LinearScale,
+    OrdinalScale,
+    QuantileScale,
+    QuantizeScale,
+    SequentialScale,
+    Signal as VegaSignal,
+    Spec,
+    UrlData,
+    ValuesData
+} from 'vega-typings/types';
 import { SandDance } from '@msrvida/sanddance-react';
 import { Signal } from '../controls/signal';
-import { Signal as VegaSignal } from 'vega-typings/types';
 import { strings } from '../language';
+
+type ScalesWithRange = QuantileScale | QuantizeScale | OrdinalScale | LinearScale | SequentialScale;
 
 export interface Props {
     explorer: Explorer;
+    dataFile: DataFile;
+    scheme: string;
 }
 
 export interface State {
     showVegaDialog: boolean;
+    dataRefType: DataRefType;
+    spec: Spec;
+}
+
+enum DataRefType {
+    none, inline, url
 }
 
 function filterSignals(signal: VegaSignal) {
@@ -31,17 +54,86 @@ function filterSignals(signal: VegaSignal) {
     }
 }
 
+function cloneData(vegaSpec: Spec) {
+    const data0 = vegaSpec.data[0];
+    const valuesData = data0 as ValuesData;
+    const values = valuesData.values;
+    delete valuesData.values;
+    const data = SandDance.VegaDeckGl.util.clone(vegaSpec.data);
+    valuesData.values = values;
+    return data;
+}
+
+function cloneScales(vegaSpec: Spec) {
+    return SandDance.VegaDeckGl.util.clone(vegaSpec.scales);
+}
+
+function serializeSpec(vegaSpec: Spec, datafile: DataFile, dataRefType: DataRefType, scheme: string) {
+    const scales = cloneScales(vegaSpec);
+    const colorScale = scales.filter(scale => scale.name === SandDance.constants.ColorScaleName)[0];
+    if (scheme.indexOf('dual_') >= 0) {
+        (colorScale as ScalesWithRange).range = SandDance.colorSchemes.filter(cs => cs.scheme === scheme)[0].colors;
+    }
+    if (dataRefType === DataRefType.inline) {
+        return { ...vegaSpec, scales };
+    }
+    const data = cloneData(vegaSpec);
+    const data0 = data[0];
+    if (dataRefType === DataRefType.none) {
+        const valuesData = data0 as ValuesData;
+        valuesData.values = [];
+    } else if (dataRefType === DataRefType.url) {
+        const urlData = data0 as UrlData;
+        urlData.url = datafile.dataUrl;
+        urlData.format = { parse: 'auto', type: datafile.type };
+    }
+    return { ...vegaSpec, data, scales };
+}
+
+function defaultDataRefType(datafile: DataFile) {
+    if (datafile.dataUrl) {
+        return DataRefType.url;
+    }
+    return DataRefType.none;
+}
+
+function initState(props: Props): State {
+    return {
+        showVegaDialog: false,
+        dataRefType: defaultDataRefType(props.dataFile),
+        spec: null
+    };
+}
+
 export class Settings extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
-        this.state = {
-            showVegaDialog: false
-        };
+        this.state = initState(props);
     }
 
     render() {
         const { props, state } = this;
         if (!props.explorer.viewer || !props.explorer.viewer.vegaSpec) return null;
+        const options: FabricTypes.IDropdownOption[] = [
+            {
+                key: DataRefType.none,
+                text: strings.selectVegaSpecDataNone,
+                selected: this.state.dataRefType === DataRefType.none,
+                data: DataRefType.none
+            },
+            !props.dataFile.rawText && {
+                key: DataRefType.url,
+                text: strings.selectVegaSpecDataUrl,
+                selected: this.state.dataRefType === DataRefType.url,
+                data: DataRefType.url
+            },
+            {
+                key: DataRefType.inline,
+                text: strings.selectVegaSpecDataInline,
+                selected: this.state.dataRefType === DataRefType.inline,
+                data: DataRefType.inline
+            }
+        ].filter(Boolean);
         return (
             <div>
                 <Group
@@ -62,12 +154,15 @@ export class Settings extends React.Component<Props, State> {
                 >
                     <base.fabric.DefaultButton
                         text={strings.buttonShowVegaSpec}
-                        onClick={() => this.setState({ showVegaDialog: true })}
+                        onClick={() => this.setState({
+                            showVegaDialog: true,
+                            spec: serializeSpec(props.explorer.viewer.vegaSpec, props.dataFile, this.state.dataRefType, this.props.scheme)
+                        })}
                     />
                 </Group>
                 <Dialog
                     hidden={!state.showVegaDialog}
-                    onDismiss={() => this.setState({ showVegaDialog: false })}
+                    onDismiss={() => this.setState(initState(this.props))}
                     minWidth="80%"
                     title={strings.dialogTitleVegaSpec}
                     buttons={[
@@ -99,12 +194,16 @@ export class Settings extends React.Component<Props, State> {
                         )
                     ]}
                 >
-                    <pre
-                        id="sanddance-vega-spec"
-                        style={{
-                            height: `${document.body.offsetHeight * 0.5}px`
-                        }}>
-                        {JSON.stringify(props.explorer.viewer.vegaSpec, null, 2)}
+                    <Dropdown
+                        label={strings.labelVegaSpecData}
+                        options={options}
+                        onChange={(e, o) => this.setState({
+                            dataRefType: o.data,
+                            spec: serializeSpec(props.explorer.viewer.vegaSpec, props.dataFile, o.data, this.props.scheme)
+                        })}
+                    />
+                    <pre id="sanddance-vega-spec">
+                        {JSON.stringify(this.state.spec, null, 2)}
                     </pre>
                     <div>
                         {strings.labelVegaSpecNotes}
