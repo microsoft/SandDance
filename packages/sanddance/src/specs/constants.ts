9e494cb5b3a379d8ded206ec4198f36562000a62
Dan Marshall <danmar@microsoft.com>
2019-04-23 19:16:30 -0700

    reorg

--- packages/sanddance/src/specs/constants.ts
+++ packages/sanddance/src/specs/constants.ts
@@ -7,17 +7,23 @@ export const FieldNameCollapsed = "__SandDanceCollapsed";
 export const FieldNameSelected = "__SandDanceSelected";
 export const FieldNameTop = "__SandDanceTop";
 
-//These are strong names which appear in the specs
-export const ColorScaleName = "SandDanceColorScale";
-export const ColorBinCountSignal = "SandDanceColorBinCount";
+//Data names
 export const PreDataName = "SandDancePreData";
 export const DataName = "SandDanceData";
 export const EmptyBinDataName = "SandDanceEmptyBins";
 export const TopLookupDataName = "SandDanceTop";
 export const LegendDataName = "SandDanceLegend";
+export const FacetGroupCellDataName = "SandDanceFacetGroupCellData";
+export const FacetCellTitles = "SandDanceFacetCellTitles";
+
+//Scale names
+export const ColorScaleName = "SandDanceColorScale";
 export const MainXScale = "SandDanceMainXScale";
 export const MainYScale = "SandDanceMainYScale";
 export const MainZScale = "SandDanceMainZScale";
+
+//Signal names
+export const ColorBinCountSignal = "SandDanceColorBinCount";
 export const PointSizeSignal = "SandDancePointSize";
 export const BinXSignal = "SandDanceBinX";
 export const BinYSignal = "SandDanceBinY";
@@ -25,8 +31,6 @@ export const YDomainSignal = "SandDanceYDomainSignal";
 export const TreeMapMethod = "SandDanceTreeMapMethodSignal";
 export const FacetColumnsSignal = "SandDanceFacetColumnsSignal";
 export const FacetRowsSignal = "SandDanceFacetRowsSignal";
-export const FacetGroupCellDataName = "SandDanceFacetGroupCellData";
-export const FacetCellTitles = "SandDanceFacetCellTitles";
 export const TextScaleSignal = "SandDanceTextScale";
 export const TextSizeSignal = "SandDanceTextSize";
 export const TitleTextSizeSignal = "SandDanceTitleTextSize";
