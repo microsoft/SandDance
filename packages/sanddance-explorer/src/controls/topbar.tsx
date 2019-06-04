e307797e1eba58ce26bed92aa172bb288a4b23ac
Dan Marshall <danmar@microsoft.com>
2019-06-03 17:45:38 -0700

    removed inline style

--- packages/sanddance-explorer/src/controls/topbar.tsx
+++ packages/sanddance-explorer/src/controls/topbar.tsx
@@ -92,7 +92,7 @@ export function Topbar(props: Props) {
 
     return (
         <div className="sanddance-explorer-topbar">
-            <div className="logo" style={{ width: "250px" }}>
+            <div className="logo">
                 <Logo />
                 <a href={props.logoClickUrl || "/SandDance/"} target={props.logoClickTarget || '_blank'}>{strings.appName}</a>
             </div>
