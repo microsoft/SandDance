60b1f878032cfb3e11569017634c1a4831fb35ee
Dan Marshall <danmar@microsoft.com>
2019-06-13 16:29:20 -0700

    added embed

---
+++ packages/sanddance-explorer/embed/fabric.d.ts
@@ -0,0 +1,3 @@
+export { FabricComponents } from "@msrvida/office-ui-fabric-react-cdn-typings";
+
+export as namespace _Fabric;
---
+++ packages/sanddance-explorer/embed/sanddance-explorer.d.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+export * from '../dist/es6';
+
+export as namespace SandDanceExplorer;
