a6abb3ac015eb544c8858fc4916a6e50f2ba68ae
Rebecca Wang <rebeccarxwang@gmail.com>
2019-07-09 15:06:07 -0700

    Pass data from Azdata to SandDance

--- packages/azdata-sanddance/src/extension.ts
+++ packages/azdata-sanddance/src/extension.ts
@@ -17,11 +17,8 @@ interface WebViewWithUri {
     uriFsPath: string;
 }
 let current: WebViewWithUri | undefined = undefined;
-//let insight: azdata.Insight;
 
 export function activate(context: vscode.ExtensionContext) {
-    //let icon = azdata.DataProviderType.IconProvider;
-    //let test = azdata.dataprotocol.registerIconProvider(icon);
     console.log("----------------------------");
     context.subscriptions.push(
         vscode.commands.registerCommand('sandance.view', (commandContext: vscode.Uri | azdata.ObjectExplorerContext) => {
@@ -38,76 +35,27 @@ export function activate(context: vscode.ExtensionContext) {
             }
         )
     );
-
+    
+    //To do: replace regiserCommand with suubscription.push
 	vscode.commands.registerCommand('query.sandance', () => {
         iconclicked = !iconclicked;
         azdata.queryeditor.registerQueryEventListener({
             async onQueryEvent(type: azdata.queryeditor.QueryEvent, document: azdata.queryeditor.QueryDocument, args: any) {
                 if (type === 'visualize') {
-                    if (iconclicked) {           
+                    if (iconclicked) {       
                         const providerid = document.providerId;
                         let provider: azdata.QueryProvider;
                         provider = azdata.dataprotocol.getProvider(providerid, azdata.DataProviderType.QueryProvider);
                         let data = await provider.getQueryRows({
                             ownerUri: document.uri,
-                            batchIndex: 0,
-                            resultSetIndex: 0,
+                            batchIndex: args.batchId,
+                            resultSetIndex: args.id,
                             rowsStartIndex: 0,
-                            rowsCount: 500
+                            rowsCount: args.rowCount
                         });
                         let datavalue = data.resultSubset.rows;
                         let fileuri =  saveTemp(datavalue);
-                        viewInSandance(fileuri,context);     
-                        //let sdtab = azdata.window.createTab('Visualizer');
-                        /*
-                        sdtab.registerContent(async view => {
-                            let properties: azdata.WebViewProperties = {
-                                options: {
-                                enableScripts: true,
-                                // Only allow the webview to access resources in our extension's media directory
-                                localResourceRoots: [
-                                    vscode.Uri.file(path.join(context.extensionPath, 'resources'))
-                                ]
-                                }
-                            };
-                            let webview = view.modelBuilder.webView().withProperties(properties).component();
-                            let flexModel = view.modelBuilder.flexContainer().component();						
-                            flexModel.addItem(webview, { flex: '1' });                     
-                            flexModel.setLayout({
-                                flexFlow: 'column',
-                                alignItems: 'stretch',
-                                height: '100%'
-                                });
-                            await view.initializeModel(flexModel);
-                            //let uri= vscode.Uri.file("file:///C:/Users/t-rewang/sample.csv"); //temp
-                            const providerid = document.providerId;
-                            let provider: azdata.QueryProvider;
-                            provider = azdata.dataprotocol.getProvider(providerid, azdata.DataProviderType.QueryProvider);
-                            let data = await provider.getQueryRows({
-                                ownerUri: document.uri,
-                                batchIndex: 0,
-                                resultSetIndex: 0,
-                                rowsStartIndex: 0,
-                                rowsCount: 500
-                            });
-                            let datavalue = data.resultSubset.rows;
-                            
-                            let j = JSON.stringify(datavalue);
-                            //let i = jsonToURI(datavalue);
-                            let fileuri =  saveTemp(datavalue);
-                            //let uri = vscode.Uri.parse(i);
-                            //let uri2 = vscode.Uri.file(i);
-                            setTimeout(() => {
-                            if (webview) {
-                                //webview.html =  printstuff(j);//r.messages.length
-                                viewInSandance(fileuri,context);
-                                //queryviewInSandance(webview,fileuri, context);                                   
-                                }
-                            }, 2000);
-                                
-                        });
-                        */
-                        //document.createQueryTab(sdtab);
+                        queryViewInSandance(fileuri,context,document);                           
                     
                         }
                     }
@@ -115,25 +63,6 @@ export function activate(context: vscode.ExtensionContext) {
             
         });
 	});
-
-	
-}
-
-function jsonToURI(json){ return encodeURIComponent(JSON.stringify(json)); }
-
-function printstuff(para: any) {
-    return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SandDance</title>
-</head>
-<body>
-    <div id="app"></div>
-    ${para}
-</body>
-</html>`;
 }
 
 async function downloadAndViewInSandance(commandContext: azdata.ObjectExplorerContext, context: vscode.ExtensionContext): Promise<void> {
@@ -186,30 +115,45 @@ function viewInSandance(fileUri: vscode.Uri, context: vscode.ExtensionContext): 
     }
 }
 
-
-function queryviewInSandance(webview: azdata.WebViewComponent,fileUri: vscode.Uri , context: vscode.ExtensionContext): void {
-    //const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
-    // Otherwise, create a new panel
+// View in SandDance for SQL query editor
+function queryViewInSandance(fileUri: vscode.Uri, context: vscode.ExtensionContext, editorUri: azdata.queryeditor.QueryDocument): void {
+    const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
     const uriFsPath = fileUri.fsPath;
-    webview.html = getWebviewContent(context.extensionPath, uriFsPath);
-    // Handle messages from the webview
-        webview.onMessage(message => {
+    //only allow one SandDance at a time
+    if (current && current.uriFsPath !== uriFsPath) {
+        current.panel.dispose();
+        current = undefined;
+    }
+    if (current) {
+        //TODO: registerWebviewPanelSerializer to hydrate state
+        // If we already have a panel, show it in the target column
+        current.panel.reveal(columnToShowIn);
+    }
+    else {
+        // Otherwise, create a new panel
+        const uriTabName = editorUri.uri;
+        current = newPanelQuery(context, uriFsPath, uriTabName);
+        current.panel.onDidDispose(() => {
+            current = undefined;
+        }, null, context.subscriptions);
+        // Handle messages from the webview
+        current.panel.webview.onDidReceiveMessage(message => {
             switch (message.command) {
                 case 'getFileContent':
                     fs.readFile(uriFsPath, (err, data) => {
+                        if (current && current.panel.visible) {
                             //TODO string type of dataFile
                             const dataFile = {
                                 type: path.extname(uriFsPath).substring(1),
                                 rawText: data.toString('utf8')
                             };
-                           webview.message({ command: 'gotFileContent', dataFile });
-                        
+                            current.panel.webview.postMessage({ command: 'gotFileContent', dataFile });
+                        }
                     });
                     break;
             }
         }, undefined, context.subscriptions);
-    
-    
+    }
 }
 
 
@@ -245,6 +189,27 @@ function newPanel(context: vscode.ExtensionContext, uriFsPath: string) {
         panel: vscode.window.createWebviewPanel(
             'sandDance',
             `SandDance: ${path.basename(uriFsPath)}`,
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true,
+                // Only allow the webview to access resources in our extension's media directory
+                localResourceRoots: [
+                    vscode.Uri.file(path.join(context.extensionPath, 'resources'))
+                ],
+                retainContextWhenHidden: true
+            }
+        ),
+        uriFsPath
+    };
+    webViewWithUri.panel.webview.html = getWebviewContent(context.extensionPath, uriFsPath);
+    return webViewWithUri;
+}
+
+function newPanelQuery(context: vscode.ExtensionContext, uriFsPath: string, uriTabName: string) {
+    const webViewWithUri: WebViewWithUri = {
+        panel: vscode.window.createWebviewPanel(
+            'sandDance',
+            `SandDance: ${uriTabName}`,
             vscode.ViewColumn.One,
             {
                 enableScripts: true,
