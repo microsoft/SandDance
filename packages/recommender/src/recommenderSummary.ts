ad194a83d82eea964161ae610a47e63c5c0fac10
Dan Marshall <danmar@microsoft.com>
2019-08-09 16:52:27 -0700

    rename to getRecommenderColumns

--- packages/recommender/src/recommenderSummary.ts
+++ packages/recommender/src/recommenderSummary.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 import * as SandDance from "@msrvida/sanddance";
-import { Recommendation, detectSequentialAll } from './recommender';
+import { Recommendation, getRecommenderColumns } from './recommender';
 import { BarChartRecommenderSummary } from './barChart';
 import { ScatterPlotRecommenderSummary } from './scatterPlot';
 
@@ -9,7 +9,7 @@ export class RecommenderSummary {
     public rec: Recommendation;
 
     constructor(columns: SandDance.types.Column[], data: object[]) {
-        let recommenderColumns = detectSequentialAll(columns, data);
+        let recommenderColumns = getRecommenderColumns(columns, data);
         let quickRec: Recommendation = new ScatterPlotRecommenderSummary(recommenderColumns, data).recommend();
         if (quickRec) this.rec = quickRec;
         else {
