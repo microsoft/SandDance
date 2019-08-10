6c21eada5b4c8c4592a1403cd8556cb997a16a68
Dan Marshall <danmar@microsoft.com>
2019-08-09 19:35:12 -0700

    add geo and treemap

--- packages/recommender/src/index.ts
+++ packages/recommender/src/index.ts
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 export * from './barChart';
+export * from './geo';
 export * from './scatterPlot';
+export * from './treemap';
 export * from './recommenderSummary';
---
+++ packages/recommender/src/treemap.ts
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+import * as SandDance from '@msrvida/sanddance';
+import { isGeo } from './geo';
+
+export function preferredColumnForTreemapSize(columns: SandDance.types.Column[], strict: boolean) {
+    for (let i = 0; i < columns.length; i++) {
+        let c = columns[i];
+        if (c.quantitative) {
+            if (strict && c.stats.hasNegative) continue;
+            if (strict && c.stats.isSequential) continue;
+            if (strict && isGeo(c)) continue;
+            return c.name;
+        }
+    }
+}
