86649f52f693c78a5cd59757b698c6a8e410abce
Dan Marshall <danmar@microsoft.com>
2019-08-13 22:27:36 -0700

    tooltip position

--- packages/sanddance/src/tooltip.tsx
+++ packages/sanddance/src/tooltip.tsx
@@ -13,14 +13,30 @@ interface Props {
 }
 
 export class Tooltip {
-    public element: HTMLElement;
+    private element: HTMLElement;
+    private child: HTMLElement;
 
     constructor(props: Props) {
         this.element = renderTooltip(props) as any as HTMLElement;
+        this.child = this.element.children[0] as HTMLElement;
         document.body.appendChild(this.element);
         //measure and move is necessary
+        const m = this.measure();
+        if (props.position.clientX + m.width >= document.body.offsetWidth) {
+            this.child.style.right = '0px';
+        }
+        if (props.position.clientY + m.height >= document.body.offsetHeight) {
+            this.child.style.bottom = '0px';
+        }
         this.element.style.left = `${props.position.clientX}px`;
         this.element.style.top = `${props.position.clientY}px`;
+    }
+
+    private measure() {
+        const cs = getComputedStyle(this.child);
+        const height = parseFloat(cs.marginTop) + parseFloat(cs.borderTopWidth) + parseFloat(cs.height) + parseFloat(cs.borderBottomWidth) + parseFloat(cs.marginBottom);
+        const width = parseFloat(cs.marginLeft) + parseFloat(cs.borderLeftWidth) + parseFloat(cs.width) + parseFloat(cs.borderRightWidth) + parseFloat(cs.marginRight);
+        return { height, width };
     }
 
     clear() {
@@ -34,7 +50,9 @@ export class Tooltip {
 const renderTooltip = (props: Props) => {
     return (
         <div className={className(PresenterElement.tooltip, props.presenter)}>
-            tooltip
+            <div className="data-item">
+                tooltip
+            </div>
         </div>
     );
 }
