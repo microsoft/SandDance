778a72c16fe33226285861fc1e459f2106e7350a
Dan Marshall <danmar@microsoft.com>
2019-09-04 00:03:22 -0700

    expose util

--- packages/sanddance-explorer/src/index.tsx
+++ packages/sanddance-explorer/src/index.tsx
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-export { ColorSettings, DataContent, DataFile, DataFileType, Snapshot, ViewerOptions } from "./interfaces";
-export { use } from "./base";
+export { ColorSettings, DataContent, DataFile, DataFileType, Snapshot, ViewerOptions } from './interfaces';
+export { use } from './base';
+export { capabilities } from './canvas';
 export { Prefs } from './partialInsight';
 export { getColorSettingsFromThemePalette, themePalettes } from './themes'
-export * from "./explorer";
+export * from './explorer';
 
-export { SandDance } from "@msrvida/sanddance-react";
+export { SandDance, util } from '@msrvida/sanddance-react';
 export { version } from './version';
