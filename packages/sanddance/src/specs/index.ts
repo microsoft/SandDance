17073f71d2c4dc5acde06ec0df4c3db829b5a46a
Dan Marshall <danmar@microsoft.com>
2019-09-23 11:31:14 -0700

    rename to barchart-v

--- packages/sanddance/src/specs/index.ts
+++ packages/sanddance/src/specs/index.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 import * as constants from './constants';
-import { barchart } from './stableBarChart';
+import { barchart } from './barchart-v';
 import {
     Chart,
     Insight,
