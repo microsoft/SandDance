dec2aa981a311a1de36d47bea716de984a2d8417
Dan Marshall <danmar@microsoft.com>
2020-01-03 13:15:10 -0800

    allowAsyncRenderTime for refine

--- packages/sanddance/src/viewer.ts
+++ packages/sanddance/src/viewer.ts
@@ -398,17 +398,17 @@ export class Viewer {
         let result: RenderResult;
         //see if refine expression has changed
         if (!searchExpression.compare(insight.filter, this.insight.filter)) {
+            const allowAsyncRenderTime = 100;
             if (insight.filter) {
                 //refining
                 result = await this._render(insight, data, options);
                 this.presenter.animationQueue(() => {
                     this.filter(insight.filter);
-                }, this.options.transitionDurations.position, { waitingLabel: 'layout before refine', handlerLabel: 'refine after layout' });
+                }, allowAsyncRenderTime, { waitingLabel: 'layout before refine', handlerLabel: 'refine after layout' });
             } else {
                 //not refining
                 this._dataScope.setFilteredData(null);
                 result = await this._render(insight, data, options);
-                const allowAsyncRenderTime = 100;
                 this.presenter.animationQueue(() => {
                     this.reset();
                 }, allowAsyncRenderTime, { waitingLabel: 'layout before reset', handlerLabel: 'reset after layout' });
