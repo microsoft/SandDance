426e7b1037d20549d8c860be12d81720e3bf3fc7
Dan Marshall <danmar@microsoft.com>
2020-01-09 14:06:35 -0800

    added aria roles

--- packages/sanddance-explorer/src/controls/scrollable.tsx
+++ packages/sanddance-explorer/src/controls/scrollable.tsx
@@ -6,11 +6,12 @@ import { util } from '@msrvida/sanddance-react';
 export interface Props {
     children: React.ReactNode;
     className?: string;
+    role?: string;
 }
 
 export function Scrollable(props: Props) {
     return (
-        <div className={util.classList('scrollable-container', props.className)}>
+        <div className={util.classList('scrollable-container', props.className)} role={props.role}>
             <div className="scrollable">
                 {props.children}
             </div>
