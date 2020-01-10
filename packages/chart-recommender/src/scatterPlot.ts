fac9accd9b891c6a4024461fcccc964336cd10a9
Dan Marshall <danmar@microsoft.com>
2020-01-09 17:39:41 -0800

    recommend scatter on x & y

--- packages/chart-recommender/src/scatterPlot.ts
+++ packages/chart-recommender/src/scatterPlot.ts
@@ -8,8 +8,6 @@ export class ScatterPlotRecommenderSummary {
     public best: Recommendation;
 
     constructor(columns: SandDance.types.Column[], data: object[]) {
-        let longi = false;
-        let lati = false;
         let rec: Recommendation = {
             chart: 'scatterplot',
             score: undefined,
@@ -18,25 +16,32 @@ export class ScatterPlotRecommenderSummary {
             view: '2d'
         };
         columns.forEach(column => {
-            if (longi === false && isLongitude(column)) {
-                longi = true;
-                rec.columns.x = column.name;
+            if (!rec.columns.x) {
+                if (column.name.toLowerCase() === 'x') {
+                    return rec.columns.x = column.name;
+                } else if (isLongitude(column)) {
+                    return rec.columns.x = column.name;
+                }
             }
-            else if (lati === false && isLatitude(column)) {
-                lati = true;
-                rec.columns.y = column.name;
+            if (!rec.columns.y) {
+                if (column.name.toLowerCase() === 'y') {
+                    return rec.columns.y = column.name;
+                } else if (isLatitude(column)) {
+                    return rec.columns.y = column.name;
+                }
             }
-            else if (!rec.columns.color && !column.stats.isSequential) {
+            if (!rec.columns.color && !column.stats.isSequential) {
                 if (column.quantitative || column.stats.distinctValueCount < maxCategoricalColors) {
                     rec.columns.color = rec.columns.sort = column.name;
                     rec.scheme = defaultColorScheme(column);
                     if (column.quantitative) {
                         rec.colorBin = 'quantile';
                     }
+                    return;
                 }
             }
         });
-        if (longi && lati) {
+        if (rec.columns.x && rec.columns.y) {
             this.best = rec;
         }
     }
