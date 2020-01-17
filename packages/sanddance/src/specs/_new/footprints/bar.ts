b129ca6739abab82a9148264080d923a714a24a7
Dan Marshall <danmar@microsoft.com>
2020-01-16 17:06:48 -0800

    header

--- packages/sanddance/src/specs/_new/footprints/bar.ts
+++ packages/sanddance/src/specs/_new/footprints/bar.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 import { Footprint, FootprintProps } from "./footprint";
 import { Orientation } from "../../interfaces";
 
--- packages/sanddance/src/specs/_new/footprints/density.ts
+++ packages/sanddance/src/specs/_new/footprints/density.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 import { Footprint, FootprintProps } from "./footprint";
 import { Scopes } from "../../interfaces";
 
