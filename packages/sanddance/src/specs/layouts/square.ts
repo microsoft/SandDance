fcad4f434c0bd1332ae6f279589d1b6a935135a1
Dan Marshall <danmar@microsoft.com>
2020-01-21 13:11:36 -0800

    prep

--- packages/sanddance/src/specs/layouts/square.ts
+++ packages/sanddance/src/specs/layouts/square.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-import { LayoutProps, Layout, BuildProps } from './layout';
+import { BuildProps, Layout, LayoutProps } from './layout';
 import { InnerScope } from '../interfaces';
+import { Mark } from 'vega-typings';
 
 export interface SquareProps extends LayoutProps {
     growDirection: 'right-down' | 'right-up' | 'down-right';
@@ -11,6 +12,41 @@ export class Square extends Layout {
     public props: SquareProps & BuildProps;
 
     public build(): InnerScope {
-        return null;
+        const { props } = this;
+        const { global, parent } = props;
+        const name = `square_${this.id}`;
+        const facetDataName = `facet_${name}`;
+        const mark: Mark = {
+            style: 'cell',
+            name,
+            type: 'group',
+            from: {
+                data: parent.dataName
+            },
+            encode: {
+                update: {
+                    x: {
+                        value: 0
+                    },
+                    y: {
+                        value: 0
+                    },
+                    height: {
+                        value: 50
+                    },
+                    width: {
+                        value: 50
+                    }
+                }
+            },
+            marks: []
+        };
+        parent.scope.marks.push(mark);
+
+        return {
+            dataName: facetDataName,
+            scope: mark,
+            sizeSignals: parent.sizeSignals
+        };
     }
 }
--- packages/sanddance/src/specs/layouts/treemap.ts
+++ packages/sanddance/src/specs/layouts/treemap.ts
@@ -1,11 +1,42 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-import { Layout, LayoutProps } from "./layout";
+import { BuildProps, Layout, LayoutProps } from './layout';
+import { InnerScope } from '../interfaces';
+import { Mark } from 'vega-typings';
 
 export interface TreemapProps extends LayoutProps {
     corner: 'top-left' | 'bottom-left';
 }
 
 export class Treemap extends Layout {
-    //TODO implement corner
+    public props: TreemapProps & BuildProps;
+
+    public build(): InnerScope {
+        const { props } = this;
+        const { global, groupby, parent } = props;
+        const name = `square_${this.id}`;
+        const facetDataName = `facet_${name}`;
+        const mark: Mark = {
+            style: 'cell',
+            name: 'X',
+            type: 'group',
+            from: {
+                data: parent.dataName
+            },
+
+            //TODO implement corner
+
+            encode: {
+            },
+            marks: []
+        };
+        parent.scope.marks.push(mark);
+
+        return {
+            dataName: facetDataName,
+            scope: mark,
+            sizeSignals: parent.sizeSignals
+        };
+    }
+
 }
