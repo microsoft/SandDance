24c13bc5a2afa886e9bf3be5bf3052c8352ebca6
Dan Marshall <danmar@microsoft.com>
2020-01-22 12:11:14 -0800

    move array utils into vega-deck.gl

--- packages/sanddance/src/array.ts
+++ packages/sanddance/src/array.ts
@@ -1,14 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-/**
- * Returns array with items which are truthy.
- * @param args array or arrays to concat into a single array.
- */
-export function allTruthy<T>(...args: T[][]) {
-    return args.reduce((p, c) => c ? p.concat(c) : p, []).filter(Boolean);
-}
+import { util } from '@msrvida/vega-deck.gl';
 
-export function push<T>(arr: T[], items: T[]) {
-    arr.push.apply(arr, items);
-}
+const { allTruthy, concat, push } = util;
+
+export { allTruthy, concat, push };
