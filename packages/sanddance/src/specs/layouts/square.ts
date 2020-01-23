ce83793723e141e1fa88b08bbaea4664bb9eb1eb
Dan Marshall <danmar@microsoft.com>
2020-01-22 18:42:11 -0800

    working squares

--- packages/sanddance/src/specs/layouts/square.ts
+++ packages/sanddance/src/specs/layouts/square.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 import { BuildProps, Layout, LayoutProps } from './layout';
 import { InnerScope } from '../interfaces';
-import { Mark } from 'vega-typings';
+import { Mark, GroupEncodeEntry } from 'vega-typings';
 import { push } from '../../array';
 
 export interface SquareProps extends LayoutProps {
@@ -12,93 +12,122 @@ export interface SquareProps extends LayoutProps {
 
 export class Square extends Layout {
     public props: SquareProps & BuildProps;
+    private names: {
+        dataName: string,
+        aspect: string,
+        bandWidth: string,
+        squaresPerBand: string,
+        index: string,
+        gap: string,
+        size: string
+    }
 
     public build(): InnerScope {
         const { props } = this;
-        const { global, maxSignal, parent } = props;
+        const { fillDirection, global, parent } = props;
+        let { maxSignal } = props;
         const name = `square_${this.id}`;
-        const dataName = `facet_${name}`;
-        const aspect = `${name}_aspect`;
-        const squaresPerBand = `${name}_squares_per_band`;
+        this.names = {
+            dataName: `facet_${name}`,
+            aspect: `${name}_aspect`,
+            bandWidth: this.getBandWidth(),
+            squaresPerBand: `${name}_squares_per_band`,
+            index: `${name}_index`,
+            gap: `${name}_gap`,
+            size: `${name}_size`
+        };
+        const { names } = this;
         const mark: Mark = {
-            style: 'cell',
             name,
-            type: 'group',
-            // from: {
-            //     data: dataName
-            // },
+            type: 'rect',
+            from: {
+                data: names.dataName
+            },
             encode: {
                 update: {
-                    x: {
-                        signal: `datum.row_number * 1.1`
-                    },
-                    y: {
-                        value: 0
-                    },
+                    ...this.groupEncodeEntry(),
                     height: {
-                        value: 1
+                        signal: names.size
                     },
                     width: {
-                        value: 1
+                        signal: names.size
                     }
                 }
-            },
-            marks: [
-                {
-                    type: 'text',
-                    encode: {
-                        update: {
-                            text: {
-                                signal: `${parent.sizeSignals.width} + ' ' + ${parent.sizeSignals.height}`
-                            }
-                        }
-                    }
-                }
-            ]
+            }
         };
         parent.scope.marks.push(mark);
 
         parent.scope.data = parent.scope.data || [];
         parent.scope.data.push({
-            name: dataName,
+            name: names.dataName,
             source: parent.dataName,
             transform: [
                 {
                     type: 'window',
                     ops: ['row_number'],
-                    as: ['row_number']
+                    as: [names.index]
                 }
             ]
         });
 
-        // global.scope.data.push({
-        //     name: "seq",
-        //     transform: [
-        //         {
-        //             type: "sequence",
-        //             start: { signal: `${binSignalName}.start` },
-        //             stop: { signal: `${binSignalName}.stop` },
-        //             step: { signal: `${binSignalName}.step` }
-        //         }
-        //     ]
-        // });
-        //global.scope.scales.push()
-
+        if (!maxSignal) {
+            maxSignal = `length(data(${JSON.stringify(parent.dataName)}))`;
+        }
         push(global.scope.signals, [
             {
-                name: aspect,
+                name: names.aspect,
                 update: `${parent.sizeSignals.width}/${parent.sizeSignals.height}`
             },
             {
-                name: squaresPerBand,
-                update: `ceil(sqrt(${maxSignal}*${aspect}))`
+                name: names.squaresPerBand,
+                update: `ceil(sqrt(${maxSignal}*${names.aspect}))`
+            },
+            {
+                name: names.gap,
+                update: `min(0.1*(${names.bandWidth}/(${names.squaresPerBand}-1)),1)`
+            },
+            {
+                name: names.size,
+                update: `${names.bandWidth}/${names.squaresPerBand}-${names.gap}`
             }
         ]);
 
         return {
-            dataName,
-            scope: mark,
-            sizeSignals: parent.sizeSignals
+            dataName: names.dataName,
+            mark,
+            sizeSignals: {
+                height: names.size,
+                width: names.size
+            }
         };
+    }
+
+    private getBandWidth() {
+        const { sizeSignals } = this.props.parent;
+        switch (this.props.fillDirection) {
+            case 'down-right':
+                return sizeSignals.height;
+            default:
+                return sizeSignals.width;
+        }
+    }
+
+    private groupEncodeEntry(): GroupEncodeEntry {
+        const { names } = this;
+        const compartment = `${names.bandWidth}/${names.squaresPerBand}*((datum[${JSON.stringify(names.index)}]-1)%${names.squaresPerBand})`;
+        const level = `floor((datum[${JSON.stringify(names.index)}]-1)/${names.squaresPerBand})`;
+        switch (this.props.fillDirection) {
+            case 'right-up':
+            default: {
+                return {
+                    x: {
+                        signal: compartment
+                    },
+                    y: {
+                        signal: `-${names.size}-${level}*(${names.size}+${names.gap})`
+                    }
+                };
+            }
+        }
     }
 }
