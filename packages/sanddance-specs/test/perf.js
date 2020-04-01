f981e86aad2e282912b87a256edfcd3ee4e50442
Dan Marshall <danmar@microsoft.com>
2020-03-31 17:29:40 -0700

    added multiple tests

--- packages/sanddance-specs/test/perf.js
+++ packages/sanddance-specs/test/perf.js
@@ -2,9 +2,11 @@
 // Licensed under the MIT license.
 import { cloneVegaSpecWithData, getColumnsFromData, getSpecColumns } from '../dist/es6';
 import * as vega from 'vega';
+import { writeFileSync } from 'fs';
 
 vega.loader().load('../../docs/sample-data/demovote.tsv').then(tsv_data => {
     const data = vega.read(tsv_data, { type: 'tsv', parse: 'auto' });
+    const columns = getColumnsFromData(vega.inferTypes, data);
 
     const specViewOptions = {
         colors: {
@@ -17,40 +19,117 @@ vega.loader().load('../../docs/sample-data/demovote.tsv').then(tsv_data => {
         tickSize: 10
     };
 
-    const insight = {
-        "columns": {
-            "x": "Longitude",
-            "y": "Latitude",
-            "color": "Obama",
-            "z": "Education",
-            "sort": "State",
-            "facet": "MedAge"
+    const insightPasses = [
+        {
+            name: "scatter",
+            insight: {
+                "columns": {
+                    "x": "Longitude",
+                    "y": "Latitude",
+                    "color": "Obama",
+                    "z": "Education"
+                },
+                "scheme": "redblue",
+                "size": {
+                    "height": 600,
+                    "width": 800
+                },
+                "chart": "scatterplot",
+                "view": "3d"
+            }
         },
-        "facetStyle": "wrap",
-        "scheme": "redblue",
-        "size": {
-            "height": 600,
-            "width": 800
+        ,
+        {
+            name: "stacks not faceted",
+            insight: {
+                "columns": {
+                    "x": "Longitude",
+                    "y": "Latitude",
+                    "color": "Obama",
+                    "z": "Education",
+                    "sort": "State"
+                },
+                "scheme": "redblue",
+                "size": {
+                    "height": 600,
+                    "width": 800
+                },
+                "chart": "stacks",
+                "view": "3d"
+            }
         },
-        "chart": "stacks",
-        "view": "3d"
-    };
+        {
+            name: "stacks facet wrap",
+            insight: {
+                "columns": {
+                    "x": "Longitude",
+                    "y": "Latitude",
+                    "color": "Obama",
+                    "z": "Education",
+                    "sort": "State",
+                    "facet": "MedAge"
+                },
+                "facetStyle": "wrap",
+                "scheme": "redblue",
+                "size": {
+                    "height": 600,
+                    "width": 800
+                },
+                "chart": "stacks",
+                "view": "3d"
+            }
+        },
+        {
+            name: "stacks facet cross",
+            insight: {
+                "columns": {
+                    "x": "Longitude",
+                    "y": "Latitude",
+                    "color": "Obama",
+                    "z": "Education",
+                    "sort": "State",
+                    "facet": "MedAge",
+                    "facetV": "Income"
+                },
+                "facetStyle": "cross",
+                "scheme": "redblue",
+                "size": {
+                    "height": 600,
+                    "width": 800
+                },
+                "chart": "stacks",
+                "view": "3d"
+            }
+        }
+    ];
 
-    const columns = getColumnsFromData(vega.inferTypes, data);
+    insightPasses.forEach(pass => {
+        const { insight, name } = pass;
+        const context = { specColumns: getSpecColumns(insight, columns), insight, specViewOptions };
 
-    const context = { specColumns: getSpecColumns(insight, columns), insight, specViewOptions };
+        const specResult = cloneVegaSpecWithData(context, data);
 
-    const specResult = cloneVegaSpecWithData(context, data);
+        if (specResult.errors) {
+            console.log(errors);
+        } else {
+            const runtime = vega.parse(specResult.vegaSpec);
+            const vegaView = new vega.View(runtime);
+            const startTime = new Date();
+            vegaView.runAsync().then(() => {
+                const stopTime = new Date();
+                console.log(`${name} done in ${stopTime - startTime}`);
 
-    if (specResult.errors) {
-        console.log(errors);
-    } else {
-        const runtime = vega.parse(specResult.vegaSpec);
-        const vegaView = new vega.View(runtime);
-        const startTime = new Date();
-        vegaView.runAsync().then(() => {
-            const stopTime = new Date();
-            console.log(`done in ${stopTime - startTime}`);
-        });
-    }
+                const d0 = specResult.vegaSpec.data[0];
+
+                delete d0.values;
+                d0.url = "https://sanddance.js.org/sample-data/demovote.tsv";
+                d0.format = {
+                    "parse": "auto",
+                    "type": "tsv"
+                };
+
+                writeFileSync(`${name}.vg.json`, JSON.stringify(specResult.vegaSpec, null, 2), 'utf8');
+            });
+        }
+    });
 });
