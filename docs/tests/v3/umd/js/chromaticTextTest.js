981a995fc05d67a52b98f32469bbcbb5199edede
Dan Marshall <danmar@microsoft.com>
2020-04-03 00:22:39 -0700

    v3 deployment

---
+++ docs/tests/v3/umd/js/chromaticTextTest.js
@@ -0,0 +1,74 @@
+var chromaticTextTest;
+(function (chromaticTextTest) {
+    SandDance.use(null, deck, deck, luma);
+    var colors = {
+        red: [255, 0, 0],
+        green: [0, 255, 0],
+        blue: [0, 0, 255],
+        orange: [255, 165, 0],
+        black: [0, 0, 0]
+    };
+    chromaticTextTest.presenter = new SandDance.VegaDeckGl.Presenter(document.querySelector('#vis'));
+    var stage = {
+        view: '2d',
+        cubeData: [],
+        axes: { x: [], y: [] },
+        legend: { rows: {} },
+        gridLines: [],
+        textData: [
+            {
+                color: colors.red,
+                text: 'red',
+                position: [-100, 0, 0],
+                size: 500,
+                textAnchor: 'middle'
+            },
+            {
+                color: colors.green,
+                text: 'green',
+                position: [0, 0, 0],
+                size: 500,
+                textAnchor: 'middle'
+            },
+            {
+                color: colors.blue,
+                text: 'blue',
+                position: [100, 0, 0],
+                size: 500,
+                textAnchor: 'middle'
+            },
+            {
+                color: colors.black,
+                text: 'black',
+                position: [0, 0, -20],
+                size: 500,
+                textAnchor: 'middle'
+            }
+        ]
+    };
+    chromaticTextTest.presenter.present(stage, 0, 0, {
+        getTextHighlightColor: function (t) {
+            switch (t.text) {
+                case 'red':
+                    return colors.green;
+                case 'green':
+                    return colors.red;
+                case 'blue':
+                    return colors.orange;
+            }
+            return colors.black;
+        },
+        onTextClick: function (e, t) {
+            alert(t.text + ' clicked');
+        }
+    });
+    var orbitViewState = {
+        distance: 10,
+        fov: 60,
+        lookAt: [0, 0, 0],
+        rotationOrbit: 0,
+        rotationX: 90,
+        zoom: 0.05
+    };
+    chromaticTextTest.presenter.deckgl.setProps({ viewState: orbitViewState });
+})(chromaticTextTest || (chromaticTextTest = {}));
---
+++ docs/tests/v3/umd/js/test.js
@@ -0,0 +1,26 @@
+var test;
+(function (test) {
+    var data = [
+        { myUid: 0, myColor: 0, mySort: 0, myX: 0, myY: 0, myZ: 0 },
+        { myUid: 1, myColor: 1, mySort: 1, myX: 1, myY: 1, myZ: 1 },
+        { myUid: 2, myColor: 2, mySort: 2, myX: 2, myY: 2, myZ: 2 }
+    ];
+    var options = {
+        columns: {
+            color: 'myColor',
+            sort: 'mySort',
+            uid: 'myUid',
+            x: 'myX',
+            y: 'myY',
+            z: 'myZ'
+        },
+        size: {
+            height: 700,
+            width: 700
+        },
+        chart: 'scatterplot'
+    };
+    SandDance.use(vega, deck, deck, luma);
+    test.viewer = new SandDance.Viewer(document.querySelector('#vis'));
+    test.viewer.render(options, data);
+})(test || (test = {}));
