a373a7adcfc3a607c97e96b503984705e7ab7f8c
Mat Sz <contact@matsz.dev>
2020-05-19 19:18:22 +0200

    updated dependencies

--- packages/sanddance-vue/src/SandDanceVue.vue
+++ packages/sanddance-vue/src/SandDanceVue.vue
@@ -1,11 +1,17 @@
 <template>
+    <div>
+
+    </div>
 </template>
 
 <style type="text/css">
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
 @import "../../node_modules/@msrvida/sanddance/dist/css/sanddance";
 </style>
 
 <script>
+export default {
+  name: 'SandDanceVue',
+  props: {
+  },
+};
 </script>
