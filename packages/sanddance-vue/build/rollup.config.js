36533fb86a984872472e22213279b1fa39d0595b
Mat Sz <contact@matsz.dev>
2020-05-19 18:42:25 +0200

    added a basic vue component project

---
+++ packages/sanddance-vue/build/rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from 'rollup-plugin-commonjs';
+import vue from 'rollup-plugin-vue';
+import buble from 'rollup-plugin-buble';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: 'src/wrapper.js',
+    output: {
+        name: 'SandDanceVue',
+        exports: 'named',
+    },
+    plugins: [
+        typescript(),
+        commonjs(),
+        vue({
+            css: true,
+            compileTemplate: true,
+        }),
+        resolve(),
+        buble(),
+    ],
+};
