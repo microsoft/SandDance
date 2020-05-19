3ec32064e6dc1768a9ffd326adb83e61408ef064
Mat Sz <contact@matsz.dev>
2020-05-19 18:42:25 +0200

    added a basic vue component project

---
+++ packages/sanddance-vue/src/SandDanceVue.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<style type="text/css">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+@import "../../node_modules/@msrvida/sanddance/dist/css/sanddance";
+</style>
+
+<script>
+</script>
