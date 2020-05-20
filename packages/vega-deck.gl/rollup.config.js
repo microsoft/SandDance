59700bc9b19cc5f4fe7c5bf9ef5f243ca8c84f86
Dan Marshall <danmar@microsoft.com>
2020-05-20 16:28:21 -0700

    update rollup to v1 (#231)

--- packages/vega-deck.gl/rollup.config.js
+++ packages/vega-deck.gl/rollup.config.js
@@ -1,6 +1,6 @@
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: './dist/es6/index.js',
