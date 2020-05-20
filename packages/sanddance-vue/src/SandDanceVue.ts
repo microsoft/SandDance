1c8f351dfe52f0be35d6e30468846fcdb8d68b91
Mat Sz <contact@matsz.dev>
2020-05-20 18:43:33 +0200

    added css, fixed issues

--- packages/sanddance-vue/src/SandDanceVue.ts
+++ packages/sanddance-vue/src/SandDanceVue.ts
@@ -2,6 +2,8 @@ import Vue, { PropType } from 'vue';
 import { deepCompare } from './util';
 import { specs, types, VegaDeckGl, Viewer } from '@msrvida/sanddance';
 
+import '../node_modules/@msrvida/sanddance/dist/css/sanddance.css';
+
 function addNullable(insight: specs.Insight, signalValues: specs.SignalValues) {
     const withNulls: specs.Insight = { view: null, filter: null, ...insight, signalValues };
     return withNulls;
@@ -12,31 +14,33 @@ export default Vue.extend({
     props: {
         viewerOptions: Object as PropType<Partial<types.ViewerOptions>>,
         insight: Object as PropType<specs.Insight>,
-        viewerData: Object as PropType<Object[]>,
+        data: Array as PropType<Object[]>,
         renderOptions: Object as PropType<types.RenderOptions>,
     },
-    data: {
-        viever: new Viewer(this.$refs.vieverDiv, this.vieverOptions),
-        lastData: [],
+    data: function () {
+        return {
+            viewer: undefined,
+            lastData: [],
+        };
     },
     computed:{
-        needsLayout: () => {
-            return this.insight && this.data && this.areLayoutPropsSame;
+        needsLayout: function () {
+            return this.insight && this.data && !this.areLayoutPropsSame;
         },
-        areLayoutPropsSame: () => {
+        areLayoutPropsSame: function () {
             const currentInsight = this.viewer.getInsight();
             const a = addNullable(currentInsight, { ...this.viewer.insight.signalValues, ...currentInsight.signalValues });
             const b = addNullable(this.insight, { ...a.signalValues, ...this.insight.signalValues });
             const compare = deepCompare(a, b);
-            return compare && (this.viewerData === this.lastData);
+            return compare && (this.data === this.lastData);
         }
     },
     methods: {
-        layout: () => {
-            this.lastData = this.viewerData;
+        layout: function () {
+            this.lastData = this.data;
             this.viewer.render(
                 this.insight,
-                this.viewerData,
+                this.data,
                 this.renderOptions
             ).then(renderResult => {
                 this.$emit('view', renderResult);
@@ -44,14 +48,16 @@ export default Vue.extend({
                 this.$emit('error', e);
             });
         },
-        view: () => {
+        view: function () {
             if (this.needsLayout) {
                 this.layout();
             }
         }
     },
     mounted: function () {
+        this.viewer = new Viewer(this.$refs.viewerDiv as HTMLElement, this.viewerOptions);
         this.$emit('mounted', this.viewer.presenter.getElement(VegaDeckGl.PresenterElement.gl));
+        this.view();
     },
     updated: function () {
         this.viewer.options = VegaDeckGl.util.deepMerge(this.viewer.options, this.viewerOptions) as types.ViewerOptions;
