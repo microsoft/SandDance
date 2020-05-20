35015cd775145c973a90b682e4abd72e9e3dfe45
Mat Sz <contact@matsz.dev>
2020-05-20 17:51:47 +0200

    added suppression of common warning message

--- packages/sanddance-vue/build/rollup.config.js
+++ packages/sanddance-vue/build/rollup.config.js
@@ -10,6 +10,15 @@ export default {
         name: 'SandDanceVue',
         exports: 'named',
     },
+    onwarn: function (warning) {
+        // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
+        // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+        if (/The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten/.test(warning.message)) {
+            return;
+        }
+
+        console.warn(warning.message);
+    },
     plugins: [
         commonjs(),
         vue({
