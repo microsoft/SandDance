5aee4468e197605ea3f285c57da0eb834f7acd8b
Dan Marshall <danmar@microsoft.com>
2020-06-03 15:10:07 -0700

    build single file for extension frontend

--- extensions/common-frontend/src/app.tsx
+++ extensions/common-frontend/src/app.tsx
@@ -8,133 +8,173 @@ declare var deck: VegaDeckGl.types.DeckBase & VegaDeckGl.types.DeckLayerBase;
 declare var luma: VegaDeckGl.types.LumaBase;
 declare var FluentUIReact: _FluentUI.FluentUIComponents;
 
-SandDanceExplorer.use(FluentUIReact, vega, deck, deck, luma);
+namespace SandDanceApp {
+    SandDanceExplorer.use(FluentUIReact, vega, deck, deck, luma);
 
-function getTextcolor() {
-    return getComputedStyle(document.body).color;
-}
-
-function getThemePalette(darkTheme: boolean) {
-    const theme = darkTheme ? 'dark-theme' : '';
-    return SandDanceExplorer.themePalettes[theme];
-}
-
-function getViewerOptions(darkTheme: boolean) {
-    const color = getTextcolor();
-    const viewerOptions: Partial<SandDanceExplorer.ViewerOptions> = {
-        colors: {
-            ...SandDanceExplorer.getColorSettingsFromThemePalette(getThemePalette(darkTheme)),
-            axisLine: color,
-            axisText: color,
-            hoveredCube: color
-        }
-    };
-    return viewerOptions;
-}
-
-function getVscodeThemeClassName() {
-    const classList = [].slice.apply(document.body.classList) as string[];
-    const prefixed = classList.filter(className => className.indexOf('vscode') === 0);
-    if (prefixed.length) {
-        return prefixed[0];
+    function getTextcolor() {
+        return getComputedStyle(document.body).color;
     }
-}
 
-interface State {
-    darkTheme: boolean;
-}
+    function getThemePalette(darkTheme: boolean) {
+        const theme = darkTheme ? 'dark-theme' : '';
+        return SandDanceExplorer.themePalettes[theme];
+    }
 
-interface Handlers {
-    message: (e: MessageEvent) => void;
-    resize: (e: UIEvent) => void;
-}
-
-class App extends React.Component<{}, State> {
-    private viewerOptions: Partial<SandDance.types.ViewerOptions>;
-    private handlers: Handlers;
-    public explorer: SandDanceExplorer.Explorer;
-
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            darkTheme: null
+    function getViewerOptions(darkTheme: boolean) {
+        const color = getTextcolor();
+        const viewerOptions: Partial<SandDanceExplorer.ViewerOptions> = {
+            colors: {
+                ...SandDanceExplorer.getColorSettingsFromThemePalette(getThemePalette(darkTheme)),
+                axisLine: color,
+                axisText: color,
+                hoveredCube: color
+            }
         };
-        this.viewerOptions = getViewerOptions(this.state.darkTheme);
+        return viewerOptions;
+    }
 
-        this.handlers = {
-            message: event => {
-                // Handle the message inside the webview
-                const message = event.data as Message;
+    function getVscodeThemeClassName() {
+        const classList = [].slice.apply(document.body.classList) as string[];
+        const prefixed = classList.filter(className => className.indexOf('vscode') === 0);
+        if (prefixed.length) {
+            return prefixed[0];
+        }
+    }
 
-                switch (message.command) {
-                    case 'gotFileContent':
-                        this.explorer && this.explorer.load(message.dataFile);
+    interface State {
+        compactUI: boolean;
+        darkTheme: boolean;
+    }
 
-                        //TODO: hydrate state
+    interface Handlers {
+        message: (e: MessageEvent) => void;
+        resize: (e: UIEvent) => void;
+        keyup: (e: KeyboardEvent) => void;
+    }
 
-                        break;
+    const z = 'z'.charCodeAt(0);
+    const Z = 'Z'.charCodeAt(0);
+
+    export class App extends React.Component<{}, State> {
+        private viewerOptions: Partial<SandDance.types.ViewerOptions>;
+        private handlers: Handlers;
+        public explorer: SandDanceExplorer.Explorer;
+
+        constructor(props: {}) {
+            super(props);
+
+            this.state = {
+                compactUI: false,   //TODO get memento
+                darkTheme: null
+            };
+            this.viewerOptions = getViewerOptions(this.state.darkTheme);
+
+            this.handlers = {
+                message: event => {
+                    // Handle the message inside the webview
+                    const message = event.data as Message;
+
+                    switch (message.command) {
+                        case 'gotFileContent':
+                            this.explorer && this.explorer.load(message.dataFile);
+
+                            //TODO: hydrate state
+
+                            break;
+                    }
+                },
+                resize: e => {
+                    this.explorer && this.explorer.resize();
+                },
+                keyup: e => {
+                    //look for CTRL Z or CTRL SHIFT Z
+                    if (e.ctrlKey && (e.keyCode === z || e.keyCode === Z)) {
+                        if (e.shiftKey) {
+                            this.explorer.redo();
+                        } else {
+                            this.explorer.undo();
+                        }
+                    }
                 }
-            },
-            resize: e => {
-                this.explorer && this.explorer.resize();
-            }
-        };
-    }
+            };
+        }
 
-    private wireEventHandlers(add: boolean) {
-        for (let key in this.handlers) {
-            if (add) {
-                window.addEventListener(key, this.handlers[key]);
-            } else {
-                window.removeEventListener(key, this.handlers[key]);
+        private wireEventHandlers(add: boolean) {
+            for (let key in this.handlers) {
+                if (add) {
+                    window.addEventListener(key, this.handlers[key]);
+                } else {
+                    window.removeEventListener(key, this.handlers[key]);
+                }
             }
         }
-    }
 
-    checkForDarkTheme() {
-        let vscodeThemeClassName = getVscodeThemeClassName();
-        const darkTheme = vscodeThemeClassName.indexOf('dark') >= 0;
-        this.viewerOptions = getViewerOptions(darkTheme);
-        if (this.state.darkTheme !== darkTheme && this.explorer) {
-            this.explorer.updateViewerOptions(this.viewerOptions);
-            if (this.explorer.viewer) {
-                this.explorer.viewer.renderSameLayout(this.explorer.viewerOptions);
+        checkForDarkTheme() {
+            let vscodeThemeClassName = getVscodeThemeClassName();
+            const darkTheme = vscodeThemeClassName.indexOf('dark') >= 0;
+            this.viewerOptions = getViewerOptions(darkTheme);
+            if (this.state.darkTheme !== darkTheme && this.explorer) {
+                this.explorer.updateViewerOptions(this.viewerOptions);
+                if (this.explorer.viewer) {
+                    this.explorer.viewer.renderSameLayout(this.explorer.viewerOptions);
+                }
             }
+            FluentUIReact.loadTheme({ palette: getThemePalette(darkTheme) });
+            this.setState({ darkTheme });
         }
-        FluentUIReact.loadTheme({ palette: getThemePalette(darkTheme) });
-        this.setState({ darkTheme });
-    }
 
-    mounted(explorer: SandDanceExplorer.Explorer) {
-        this.explorer = explorer;
+        mounted(explorer: SandDanceExplorer.Explorer) {
+            this.explorer = explorer;
 
-        this.wireEventHandlers(true);
+            this.wireEventHandlers(true);
 
-        const vscode = acquireVsCodeApi();
+            const vscode = acquireVsCodeApi();
 
-        vscode.postMessage({
-            command: 'getFileContent'
-        });
+            vscode.postMessage({
+                command: 'getFileContent'
+            });
 
-        this.checkForDarkTheme();
-
-        new MutationObserver((mutationRecords) => {
             this.checkForDarkTheme();
-        }).observe(document.body, { attributeFilter: ['class'] });
+
+            new MutationObserver((mutationRecords) => {
+                this.checkForDarkTheme();
+            }).observe(document.body, { attributeFilter: ['class'] });
+        }
+
+        render() {
+            return (
+                <SandDanceExplorer.Explorer
+                    logoClickUrl="https://microsoft.github.io/SandDance/"
+                    theme={this.state.darkTheme && 'dark-theme'}
+                    viewerOptions={this.viewerOptions}
+                    initialView="2d"
+                    mounted={e => this.mounted(e)}
+                    compactUI={this.state.compactUI}
+                    additionalSettings={[
+                        {
+                            groupLabel: strings.labelPreferences,
+                            children: (
+                                <FluentUIReact.Toggle
+                                    label={strings.labelCompactUI}
+                                    title={strings.labelCompactUIDescription}
+                                    checked={this.state.compactUI}
+                                    onChange={(e, checked?) => {
+                                        if (checked) {
+                                            localStorage.setItem('compactUI', 'true');
+                                        } else {
+                                            localStorage.removeItem('compactUI');
+                                        }
+                                        this.setState({ compactUI: checked });
+                                    }}
+                                />
+                            )
+                        }
+                    ]}
+                />
+            );
+        }
     }
 
-    render() {
-        return (
-            <SandDanceExplorer.Explorer
-                logoClickUrl="https://microsoft.github.io/SandDance/"
-                theme={this.state.darkTheme && 'dark-theme'}
-                viewerOptions={this.viewerOptions}
-                initialView="2d"
-                mounted={e => this.mounted(e)}
-            />
-        );
-    }
+    const app = React.createElement(App);
+    ReactDOM.render(app, document.getElementById('app'));
 }
-
-const app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
