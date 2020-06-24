9a88fe57c3dd41b6c76a6eae83a8a431b9a5b7e5
Dan Marshall <danmar@microsoft.com>
2020-06-24 13:05:54 -0700

    rename to extension-bundle

--- extensions/azdata-sanddance/webpack.config.js
+++ extensions/azdata-sanddance/webpack.config.js
@@ -12,7 +12,7 @@ const config = {
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
+    filename: 'extension-bundle.js',
     libraryTarget: 'commonjs2',
     devtoolModuleFilenameTemplate: '../[resource-path]'
   },
