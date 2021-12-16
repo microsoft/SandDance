6949121b80a74a10b372c86affde714deb42a81e
Dan Marshall <danmar@microsoft.com>
2021-12-15 23:04:31 -0800

    const instead of let

--- packages/vega-deck.gl/src/marks/rule.ts
+++ packages/vega-deck.gl/src/marks/rule.ts
@@ -11,11 +11,10 @@ import { zSwap } from '../zaxis';
 const markStager: MarkStager = (options: MarkStagerOptions, stage: Stage, scene: Scene, x: number, y: number, groupType: GroupType) => {
 
     base.vega.sceneVisit(scene, function (item: SceneLine) {
-        var x1, y1, x2, y2;
-        x1 = item.x || 0;
-        y1 = item.y || 0;
-        x2 = item.x2 != null ? item.x2 : x1;
-        y2 = item.y2 != null ? item.y2 : y1;
+        const x1 = item.x || 0;
+        const y1 = item.y || 0;
+        const x2 = item.x2 != null ? item.x2 : x1;
+        const y2 = item.y2 != null ? item.y2 : y1;
 
         const lineItem = styledLine(x1 + x, y1 + y, x2 + x, y2 + y, item.stroke, item.strokeWidth);
 
