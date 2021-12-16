a5a46a16c2b6275fac8f5b2326b06b042003a6e9
Dan Marshall <danmar@microsoft.com>
2021-12-16 13:27:49 -0800

    remove mdx

--- packages/scroll-story/src/mdx.d.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-declare module '*.mdx' {
-    let MDXComponent: (props) => JSX.Element;
-    export default MDXComponent;
-}
