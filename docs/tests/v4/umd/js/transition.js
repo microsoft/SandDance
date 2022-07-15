4f47c311685b8ddea53ec8bed781fed9e4bfe57d
Dan Marshall <danmar@microsoft.com>
2022-07-15 11:00:36 -0700

    deploy v4

---
+++ docs/tests/v4/umd/js/transition.js
@@ -0,0 +1,53 @@
+var transition;
+(function (transition) {
+    var view;
+    var lastSpec;
+    var viewType = '3d';
+    SandDance.use(vega);
+    function toggleView() {
+        if (viewType === '3d') {
+            viewType = '2d';
+        }
+        else {
+            viewType = '3d';
+        }
+        update(lastSpec);
+    }
+    transition.toggleView = toggleView;
+    function update(spec) {
+        view = new SandDance.VegaMorphCharts.ViewGl(vega.parse(spec), { presenter: view && view.presenter, getView: function () { return viewType; } })
+            .renderer('morphcharts')
+            .initialize(document.querySelector('#split-right'))
+            .run();
+        lastSpec = spec;
+    }
+    transition.update = update;
+    function getText(textId) {
+        var textarea = document.getElementById(textId);
+        var text = textarea.value;
+        var errorDiv = document.getElementById('error');
+        var splitRight = document.getElementById('split-right');
+        try {
+            var spec = JSON.parse(text);
+            splitRight.style.opacity = '1';
+            errorDiv.style.display = 'none';
+            update(spec);
+        }
+        catch (e) {
+            errorDiv.innerText = e;
+            errorDiv.style.display = '';
+            splitRight.style.opacity = '0.1';
+        }
+    }
+    transition.getText = getText;
+    fetch('./specs/scatter3D.json')
+        .then(function (response) { return response.text(); })
+        .then(function (text) {
+        var textarea = document.getElementById('text1');
+        textarea.value = text;
+        getText('text1');
+    });
+    fetch('./specs/titanic.json')
+        .then(function (response) { return response.text(); })
+        .then(function (text) { return document.getElementById('text2').value = text; });
+})(transition || (transition = {}));
---
+++ docs/tests/v4/umd/js/treeMapTest.js
@@ -0,0 +1,23 @@
+var treeMapTest;
+(function (treeMapTest) {
+    SandDance.use(vega);
+    treeMapTest.viewer = new SandDance.Viewer(document.querySelector('#vis'));
+    var glDiv = treeMapTest.viewer.presenter.getElement(SandDance.VegaMorphCharts.PresenterElement.gl);
+    var options = {
+        columns: {
+            color: 'Class',
+            size: 'TicketCost',
+            uid: 'Name'
+        },
+        scheme: 'category10',
+        size: {
+            height: glDiv.offsetHeight,
+            width: glDiv.offsetWidth
+        },
+        chart: 'treemap'
+    };
+    vega.loader().load('../../../sample-data/titanicmaster.tsv').then(function (text) {
+        var data = vega.read(text, { type: 'tsv' });
+        treeMapTest.viewer.render(options, data);
+    });
+})(treeMapTest || (treeMapTest = {}));
