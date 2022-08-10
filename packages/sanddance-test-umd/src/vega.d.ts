8a51f3b06c017e2d0af3fa0743f031022755a4f7
Dan Marshall <danmar@microsoft.com>
2022-08-09 23:47:39 -0700

    add vega types

--- packages/sanddance-test-umd/src/vega.d.ts
+++ packages/sanddance-test-umd/src/vega.d.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-export { Spec } from '../node_modules/@msrvida/sanddance/node_modules/vega-typings';
+export { inferTypes, loader, parse, read, Spec, View } from '../node_modules/@msrvida/sanddance/node_modules/vega-typings';
 
 export as namespace Vega;
