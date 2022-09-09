e59df76e72b7bd39793aef3c246544f8a9e9bb94
Dan Marshall <danmar@microsoft.com>
2022-09-08 23:30:30 -0700

    use powerbi guid

--- packages/powerbi/scripts/deploy.js
+++ packages/powerbi/scripts/deploy.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const pbiviz = require('../pbiviz.json');
-const filename = `${pbiviz.visual.name}.${pbiviz.visual.version}.pbiviz`;
+const filename = `${pbiviz.visual.guid}.${pbiviz.visual.version}.pbiviz`;
 fs.copyFileSync(`./dist/${filename}`, `../../docs/dist/powerbi/v4/${filename}`);
