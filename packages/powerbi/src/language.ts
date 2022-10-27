147151f029429bf5ceabb156b7b6b585969d2fd6
Dan Marshall <danmar@microsoft.com>
2022-10-27 14:17:37 -0700

    add powerbi log

--- packages/powerbi/src/language.ts
+++ packages/powerbi/src/language.ts
@@ -5,6 +5,8 @@ export const language = {
     fetching: 'fetching...',
     fetched: 'loaded',
     powerBiCustomVisual: 'Power BI custom visual version',
+    powerBiCustomVisualLog: 'Power BI Log',
+    clearLog: 'clear log',
     historyActionDataChange: 'Power BI data change',
     historyActionUpdate: 'Power BI update',
     webglDisabled: 'SandDance requires a WebGL enabled browser.',
---
+++ packages/powerbi/src/log.tsx
@@ -0,0 +1,55 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import * as React from 'react';
+import { controls } from '@msrvida/sanddance-explorer';
+import { language } from './language';
+import { DefaultButton } from '@fluentui/react';
+
+export interface Props {
+    log: string[];
+    clearLog: () => void;
+}
+
+export interface State {
+    showDialog: boolean;
+}
+
+export class Log extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            showDialog: false,
+        };
+    }
+
+    render() {
+        const { props, state } = this;
+        return (
+            <li>
+                <a
+                    href='#'
+                    onClick={e => {
+                        e.preventDefault();
+                        this.setState({ showDialog: true });
+                    }}
+                >{language.powerBiCustomVisualLog}</a>
+                <controls.Dialog
+                    buttons={[
+                        <DefaultButton
+                            key='clear'
+                            iconProps={{ iconName: 'Clear' }}
+                            text={language.clearLog}
+                            onClick={props.clearLog}
+                        />
+                    ]}
+                    hidden={!state.showDialog}
+                    onDismiss={() => this.setState({ showDialog: false })}
+                >
+                    <textarea value={props.log.join('\n')} />
+                </controls.Dialog>
+            </li>
+        );
+    }
+}
