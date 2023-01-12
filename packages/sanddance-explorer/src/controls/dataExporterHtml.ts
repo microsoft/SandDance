f693987a9a0170b756a2b137d1d4fa061db06fc8
Dan Marshall <danmar@microsoft.com>
2023-01-12 13:01:01 -0800

    regen as static

--- packages/sanddance-explorer/src/controls/dataExporterHtml.ts
+++ packages/sanddance-explorer/src/controls/dataExporterHtml.ts
@@ -5,18 +5,20 @@
 
 export const embedHtml = (title: string, embed: string) => `<!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/@msrvida/sanddance-explorer@4/dist/css/sanddance-explorer.css" />
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/@msrvida/sanddance-embed@4/dist/css/sanddance-embed.css" />
 </head>
-
 <body>
+    <script src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
+    <script src="https://unpkg.com/vega@5.22/build/vega.min.js"></script>
+    <script src="https://unpkg.com/@fluentui/react@8/dist/fluentui-react.js"></script>
+    <script src="https://unpkg.com/@msrvida/sanddance-explorer@4/dist/umd/sanddance-explorer.js"></script>
     <script src="https://unpkg.com/@msrvida/sanddance-embed@4.4/dist/umd/sanddance-embed.js"></script>
-
     ${embed}
-
 </body>
-
 </html>`;
