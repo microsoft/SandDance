2806e08eedbd69f7ed66c600b2c7455fd09f5bd5
Dan Marshall <danmar@microsoft.com>
2023-01-18 19:03:18 -0800

    rebuild js

--- docs/dist/sanddance-explorer/v4/sanddance-explorer.js
+++ docs/dist/sanddance-explorer/v4/sanddance-explorer.js
@@ -37550,6 +37550,7 @@ class $0000a41cc7b5918f$export$2ec4afd9b3c16a85 {
                 else this.select(search);
             },
             onLayerClick: (e)=>{
+                this.options.onCanvasClick && this.options.onCanvasClick(e);
                 this.deselect();
             },
             onLegendClick: (e, legend, clickedIndex)=>{
@@ -37760,7 +37761,7 @@ class $0000a41cc7b5918f$export$2ec4afd9b3c16a85 {
  */ $0000a41cc7b5918f$export$2ec4afd9b3c16a85.defaultViewerOptions = (0, $74c2763994d75bb8$export$fb736e4909afb3d7);
 
 
-const $15874c145702a1a4$export$83d89fbfd8236492 = "4.0.3";
+const $15874c145702a1a4$export$83d89fbfd8236492 = "4.0.4";
 
 
 const $3b509b9541e52a8f$export$1f96ae73734a86cc = (0, $94c0add5c61b9a48$export$1f96ae73734a86cc);
--- docs/dist/sanddance/v4/sanddance.js
+++ docs/dist/sanddance/v4/sanddance.js
@@ -28125,6 +28125,7 @@ f 5/6/6 1/12/6 8/11/6`;
                     }
                 },
                 onLayerClick: (e) => {
+                    this.options.onCanvasClick && this.options.onCanvasClick(e);
                     this.deselect();
                 },
                 onLegendClick: (e, legend, clickedIndex) => {
@@ -28363,7 +28364,7 @@ f 5/6/6 1/12/6 8/11/6`;
     * Copyright (c) Microsoft Corporation.
     * Licensed under the MIT License.
     */
-    const version = '4.0.3';
+    const version = '4.0.4';
 
     /*!
     * Copyright (c) Microsoft Corporation.
