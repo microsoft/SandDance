7389a620ad601f5bf3ac72ff60a258694cea3546
Dan Marshall <danmar@microsoft.com>
2023-06-15 18:31:56 -0700

    remove uitest build step

--- packages/data-inference/uitest/vega.d.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-export { inferTypes, loader, parse, read, Spec, View } from '../node_modules/vega-typings';
-
-export as namespace Vega;
