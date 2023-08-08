6c121aa081388c7adf326c925e731483a85200ed
Dan Marshall <danmar@microsoft.com>
2023-08-08 16:00:09 -0700

    set type=module

--- packages/data-inference/scripts/deploy.js
+++ packages/data-inference/scripts/deploy.js
@@ -1,2 +1,2 @@
-const fs = require('fs');
-fs.copyFileSync('./dist/umd/data-inference.js', '../../docs/tests/data-inference/v1/js/data-inference.js');
+import { copyFileSync } from 'fs';
+copyFileSync('./dist/umd/data-inference.js', '../../docs/tests/data-inference/v1/js/data-inference.js');
