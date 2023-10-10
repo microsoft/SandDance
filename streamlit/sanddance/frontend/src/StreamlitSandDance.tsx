5966af4870be696d67ae5b0ef305d4859611c168
Dan Marshall <danmar@microsoft.com>
2023-10-10 14:55:52 -0700

    handle theme

--- streamlit/sanddance/frontend/src/StreamlitSandDance.tsx
+++ streamlit/sanddance/frontend/src/StreamlitSandDance.tsx
@@ -3,7 +3,8 @@ import {
   withStreamlitConnection,
 } from "streamlit-component-lib"
 import React, { ReactNode } from "react"
-import { Explorer, Props as ExplorerProps, SandDance } from "@msrvida/sanddance-explorer";
+import { Explorer, Explorer_Class, getColorSettingsFromThemePalette, Props as ExplorerProps, SandDance, themePalettes, ViewerOptions } from "@msrvida/sanddance-explorer";
+import { fluentUI } from './fluentUIComponents';
 
 import "@msrvida/sanddance-explorer/dist/css/sanddance-explorer.css"
 import "./StreamlitSandDance.css"
@@ -19,22 +20,35 @@ interface State {
 class StreamlitSandDance extends StreamlitComponentBase<State> {
   public state: State = { numClicks: 0 }
 
+  private _explorer: Explorer_Class | undefined = undefined;
+
+  public componentWillUnmount = (): void => {
+    this._explorer = undefined;
+  };
+
   public render = (): ReactNode => {
     // Arguments that are passed to the plugin in Python are accessible
     // via `this.props.args`. Here, we access the "name" arg.
     const records: object[] = this.props.args["records"]
+    const theme = this.props.theme?.base === "dark" ? 'dark-theme' : '';
 
     const getPartialInsight: (columns: SandDance.types.Column[]) => Partial<SandDance.specs.Insight> = (columns) => {
       return this.props.args["insight"] || {};
     };
 
+    fluentUI.loadTheme({ palette: themePalettes[theme] });
+
+    const viewerOptions = getViewerOptions(theme, this.props.args["explorerProps"]?.viewerOptions);
+
     const explorerProps: ExplorerProps = {
       compactUI: true,
       mounted: e => {
+        this._explorer = e;
         e.load(records, getPartialInsight)
       },
       theme: this.props.theme?.base === "dark" ? 'dark-theme' : '',
-      ...this.props.args["explorerProps"]
+      ...this.props.args["explorerProps"],
+      viewerOptions,
     };
 
     return (
@@ -47,3 +61,10 @@ class StreamlitSandDance extends StreamlitComponentBase<State> {
 }
 
 export default withStreamlitConnection(StreamlitSandDance)
+
+function getViewerOptions(theme: string, viewerOptions: Partial<SandDance.types.ViewerOptions>): Partial<ViewerOptions> {
+  return {
+    ...viewerOptions,
+    colors: getColorSettingsFromThemePalette(themePalettes[theme]),
+  };
+}
