903cf3be5a57e6dce966e2d02f58141c113b8e3b
Dan Marshall <danmar@microsoft.com>
2024-08-20 16:35:00 -0700

    add css

---
+++ mdvega/packages/mdvega-sanddance/src/css.d.ts
@@ -0,0 +1,5 @@
+declare module '*.css' {
+    const content: string;
+    export default content;
+  }
+  
--- mdvega/packages/mdvega-sanddance/src/index.ts
+++ mdvega/packages/mdvega-sanddance/src/index.ts
@@ -8,6 +8,8 @@ import { VegaMorphCharts } from '@msrvida/sanddance';
 import { MdVega } from '@msrvida/mdvega';
 import { sanddancePlugin } from './plugin';
 
+import styles from 'bundle-text:@msrvida/sanddance/dist/css/sanddance.css';
+
 declare const vega: VegaMorphCharts.types.VegaBase;
 declare const MdVega: MdVega;
 
@@ -16,3 +18,11 @@ use(vega);
 if (MdVega) {
     MdVega.registerMarkdownPlugin(sanddancePlugin);
 }
+
+function injectStyles(css) {
+    const style = document.createElement('style');
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+}
+
+injectStyles(styles);
