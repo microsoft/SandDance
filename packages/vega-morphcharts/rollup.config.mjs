04e7e8fd5f3a290a0adc8deda46d8955f405717d
Dan Marshall <danmar@microsoft.com>
2024-09-30 10:50:18 -0700

    update rollup (#724)
    * update rollup
    
    * rename rollup config to .mjs

---
+++ packages/vega-morphcharts/rollup.config.mjs
@@ -0,0 +1,17 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+    input: './dist/es6/index.js',
+    output: {
+        file: './dist/umd/vega-morphcharts.js',
+        format: 'umd',
+        name: 'VegaMorphCharts',
+    },
+    plugins: [
+        json(),
+        resolve({ jsnext: true }),
+        commonjs({ sourceMap: false }),
+    ],
+};
